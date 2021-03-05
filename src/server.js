8fa22dc6b43ea358ebb7c42312ee6c945b405fc7
Gabriel Castro <gabrielcastrog2@gmail.com>
2021-03-04 22:59:49 -0300

    algumas alterações para testes

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ const port = 3003
 const express = require('express')
 
 const app = express()
+app.get('/produtos',(request,response,next)=>{
+    console.log('Funcionou a função Middleware :)')//Fazendo um log para testar se a função Middleware está funcionando
+    next()
+})
 
 //Passando função middleware como callback function da resposta da requisição .get
 app.get('/produtos',(request,response,next)=>{
@@ -10,5 +14,5 @@ app.get('/produtos',(request,response,next)=>{
 })
 
 app.listen(port, () => {
-    console.log('Servidor executando na porta ',port)
+    console.log('O Servidor está sendo executado na porta:',port)
 }) // colocando a porta como 'listen'
